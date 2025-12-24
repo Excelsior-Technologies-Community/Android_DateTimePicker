8821a1336e5a8e37b29079efdb7dc57973ef5b98
S13reya <shreyavshah13@gmail.com>
2025-12-24 17:14:31 +0530

    initial commit

--- app/src/main/java/com/ext/android_datetimepicker/MainActivity.kt
+++ app/src/main/java/com/ext/android_datetimepicker/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.ext.android_datetimepicker
 
-
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
 import com.ext.android_datetimepicker.databinding.ActivityMainBinding
@@ -13,14 +14,29 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.statusBarColor = android.graphics.Color.parseColor("#000000") // black
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.setSystemBarsAppearance(
-                0, // no light icons
-                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
-            )
-        }
+
+        // Inflate binding first
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Set status bar color
+        window.statusBarColor = Color.parseColor("#000000") // black
+
+        // Handle light/dark status bar icons safely
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // Use the WindowInsetsController safely
+            window.decorView?.let { decorView ->
+                decorView.windowInsetsController?.setSystemBarsAppearance(
+                    0, // clear APPEARANCE_LIGHT_STATUS_BARS to use light icons
+                    WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                )
+            }
+        } else {
+            // For older devices, use legacy flags
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility =
+                window.decorView.systemUiVisibility and
+                        View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+        }
     }
 }
